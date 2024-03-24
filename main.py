903d50455ae8c427002f5c058e17b82e94ee03b2
Jacqueline <82245936+Jacqueline2727@users.noreply.github.com>
2024-03-24 15:29:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
 
-data = pd.read_csv("student-mat.csv", sep=";") #change the delimiter
+data = pd.read_csv("student-mat.csv", sep=";") #change delimiter
 
-#Trim the data to what we need
+#Trim data
 data = data[["G1", "G2","G3", "studytime","failures","absences","schoolsup","higher"]]
+#change binary data to integer values
 data["schoolsup"] = data["schoolsup"].map({'yes':1, 'no':0})
 data["higher"]=data["higher"].map({'yes':1,'no':0})
-print(data.head()) #first 5 instances
 
-predict = "G3" #G3 is the label - what we are looking for
-
-x = np.array(data.drop(predict, axis=1))  # Correctly dropping the G3
+predict = "G3" 
+x = np.array(data.drop(predict, axis=1))  # Drop G3
 y = np.array(data[predict])
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
@@ -22,10 +21,7 @@ x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y
 
 linear = linear_model.LinearRegression()
 linear.fit(x_train, y_train) #fit the data to find the best fit
-acc = linear.score(x_test, y_test) #returns accuracy of our model
-
-print("Coefficients: ",linear.coef_)
-print("Intercept: ", linear.intercept_)
+acc = linear.score(x_test, y_test) 
 
 predictions = linear.predict(x_test)
 
